b142929d8bb792ef598f541af861908ea331587f
vikram jagtap <imvikramjagtap@gmail.com>
2024-09-21 23:46:09 +0530

    feat add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from './store'
-import Dashboard from './components/dashboard'
-import { Toaster } from './components/ui/toaster'
-
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
+import Dashboard from "./components/dashboard";
+import { Toaster } from "./components/ui/toaster";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function MyApp() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+  ]);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-          <Dashboard />
-          <Toaster />
+        <RouterProvider router={router} />
+        <Toaster />
       </PersistGate>
     </Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
